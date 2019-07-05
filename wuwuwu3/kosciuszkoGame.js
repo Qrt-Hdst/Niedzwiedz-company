2cc54096eac7a3c21e24f365ad360d9e6d3524d3
Hamilkar247 <adonnibal96@gmail.com>
2019-07-06 01:32:59 +0200

    stara skala elementow

--- wuwuwu3/kosciuszkoGame.js
+++ wuwuwu3/kosciuszkoGame.js
@@ -7,28 +7,7 @@
     const FOOD_BORDER_COLOUR = 'darkred';
 
 
-    function startGame() {
-      myGamePiece = new component(30,30,"img_snake/kosc2.png",120,120);
-      myGameArea.start 
-    }
-
-    var myGameArea = {
-      canvas : document.createElement("canvas"),
-      start : function() {
-          this.canvas.width = 480;
-          this.canvas.height = 270;
-          this.context = this.canvas.getContext("2d");
-          document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-          this.frameNo = 0;
-          this.interval = setInterval(updateGameArea, 20);
-          },
-      clear : function() {
-          this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      },
-      stop : function() {
-          clearInterval(this.interval);
-      }
-  }
+ 
 
   function component(width, height, color, x, y, type) {
     this.type = type;
